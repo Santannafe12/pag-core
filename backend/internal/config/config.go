5378cd3ab527c33550e61801e10afb5d63611279
Santannafe <santannafe12@gmail.com>
2025-04-24 21:25:37 -0300

    Configurando banco de dados

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -3,26 +3,18 @@ package config
 import "os"
 
 type Config struct {
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
+    DBConnectionString string
 }
 
 func Load() Config {
-	return Config{
-		DBHost:     getEnv("DB_HOST", "localhost"),
-		DBPort:     getEnv("DB_PORT", "5432"),
-		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", "postgres"),
-		DBName:     getEnv("DB_NAME", "pagcore"),
-	}
+    return Config{
+        DBConnectionString: getEnv("DB_CONNECTION", "postgresql://pag-core_owner:PASSWORD@ep-proud-fog-acg6tu4u-pooler.sa-east-1.aws.neon.tech/pag-core?sslmode=require"),
+    }
 }
 
 func getEnv(key, defaultVal string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
-	}
-	return defaultVal
+    if value, exists := os.LookupEnv(key); exists {
+        return value
+    }
+    return defaultVal
 }
--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -1,29 +1,25 @@
 package database
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
+    "database/sql"
+    "log"
 
-	"github.com/Santannafe12/pag-core/backend/internal/config"
-	_ "github.com/lib/pq"
+    "github.com/Santannafe12/pag-core/backend/internal/config"
+    _ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func Connect(cfg config.Config) {
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+    var err error
+    DB, err = sql.Open("postgres", cfg.DBConnectionString)
+    if err != nil {
+        log.Fatal("Erro ao conectar no banco:", err)
+    }
 
-	var err error
-	DB, err = sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal("Erro ao conectar no banco:", err)
-	}
+    if err = DB.Ping(); err != nil {
+        log.Fatal("Erro ao acessar banco:", err)
+    }
 
-	if err = DB.Ping(); err != nil {
-		log.Fatal("Erro ao acessar banco:", err)
-	}
-
-	log.Println("Conectado ao banco com sucesso")
+    log.Println("Conectado ao banco com sucesso")
 }
